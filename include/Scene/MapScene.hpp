94ba2bf715d07e7571ea79e4b2e80e03b7fac40c
jonylu7 <jonylu7@gmail.com>
2024-03-15 11:13:43 +0800

    t

---
+++ include/Scene/MapScene.hpp
@@ -0,0 +1,55 @@
+//
+// Created by 盧威任 on 2/15/24.
+//
+
+#ifndef PRACTICALTOOLSFORSIMPLEDESIGN_DEFAULTSCENE_HPP
+#define PRACTICALTOOLSFORSIMPLEDESIGN_DEFAULTSCENE_HPP
+
+#include "Camera.hpp"
+#include "Core/Context.hpp"
+#include "GameObjectManager.hpp"
+
+#include "DrawOverlays.hpp"
+#include "Grid.hpp"
+#include "Map.hpp"
+#include "SpriteSheet.hpp"
+#include "Structure/AdvencePowerPlants.hpp"
+#include "Structure/Barracks.hpp"
+#include "Structure/IWayPointStructure.hpp"
+#include "Structure/MousOverlapTool.h"
+#include "Structure/OreRefinery.hpp"
+#include "Structure/PowerPlants.hpp"
+#include "Structure/WarFactory.hpp"
+#include "Structure/WayPoint.hpp"
+#include "UI/UI.hpp"
+#include "Util/Image.hpp"
+#include "Util/Input.hpp"
+#include "Util/Keycode.hpp"
+#include "Util/Logger.hpp"
+#include "Util/Renderer.hpp"
+
+class MapScene {
+public:
+    MapScene(){};
+    ~MapScene(){};
+
+    void Start();
+    void Update();
+    void End(){};
+
+private:
+    SpriteSheet m_SpriteSheet;
+    DrawOverlays m_testdraw;
+    CameraClass m_SceneCamera;
+    Grid testGrid;
+    GameObjectManager m_GameObjectManager;
+    Util::Renderer m_Renderer;
+    UIClass m_UI;
+    MapClass m_Map;
+    std::map<int, TileClass> m_tileSets;
+    std::vector<int> m_OgMap;
+    std::shared_ptr<SpriteSheet> m_TileSetSpriteSheet =
+        std::make_shared<SpriteSheet>();
+};
+
+#endif // PRACTICALTOOLSFORSIMPLEDESIGN_DEFAULTSCENE_HPP
---
+++ src/Scene/MapScene.cpp
@@ -0,0 +1,80 @@
+//
+// Created by 盧威任 on 2/15/24.
+//
+#include "Scene/MapScene.hpp"
+#include "DrawOverlays.hpp"
+#include <iostream>
+void MapScene::Start() {
+    // image.SetImage("../assets/sprites/Shapes/B_Box.png");
+    OccupiedID::InitID();
+    LOG_TRACE("Start");
+    /*
+    m_GameObjectList[0]->SetDrawable(
+        std::make_unique<Util::Image>("../assets/sprites/Raccoon3.jpg"));
+    m_GameObjectList[0]->SetZIndex(10);
+
+    m_GameObjectList[1]->SetDrawable(
+        std::make_unique<Util::Image>("../assets/sprites/capybara.png"));
+
+    m_Structure->SetDrawable(
+        std::make_unique<Util::Image>("../assets/sprites/barracks.png"));
+    m_Structure->SetZIndex(0);
+    m_Structure->SetObjectLocation({0, 0});
+    m_Structure->Start();
+    m_Structure->SetCurrentUpdateMode(Structure::updateMode::Moveable);
+    m_Renderer.AddChild(m_Structure);
+
+    m_Inf->Start();
+
+    for (auto i : m_GameObjectList) {
+        i->Start();
+    }
+
+    m_Renderer.AddChild(m_Structure);
+    */
+    // init map
+    m_TileSetSpriteSheet->Start("../assets/sprites/TILESET_Field.png", 24, 24,
+                                20, 0);
+
+    m_OgMap = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    m_OgMap = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::vector<std::shared_ptr<TileClass>> maps;
+    for (unsigned int i = 0; i < 10; i++) {
+        maps.push_back(std::make_shared<TileClass>("rock-" + std::to_string(i),
+                                                   false, false, true, i));
+    }
+    /* doing some weird stuff, fix is needed
+    std::vector<std::shared_ptr<TileClass>> maps =
+        m_Map.readMapAndTileSet(m_OgMap, m_tileSets);
+        */
+
+    m_Map.Init(255, 255);
+    m_UI.Start();
+    m_testdraw.Start(std::vector({glm::vec2(0.F, 0.F)}),
+                     DrawOverlays::OverlayShapes::R_CROSS);
+    // m_GameObjectManager.Start();
+}
+
+void MapScene::Update() {
+    Util::Transform trans;
+    m_Map.Draw(trans, 0);
+    m_SceneCamera.Update();
+    m_Renderer.Update();
+    m_UI.Update();
+
+    Util::Transform trans2;
+    trans2.translation = Structure::ChangeToCell(
+        MapClass::ScreenToGlobalCoord(Util::Input::GetCursorPosition()));
+
+    auto tile = m_Map.getTileByCellPosition(MapClass::GlobalCoordToCellCoord(
+        MapClass::ScreenToGlobalCoord(Util::Input::GetCursorPosition())));
+
+    if (tile->getClickable()) {
+        m_testdraw.setDrawMode(DrawOverlays::OverlayShapes::B_BOXES);
+    } else {
+        m_testdraw.setDrawMode(DrawOverlays::OverlayShapes::R_CROSS);
+    }
+
+    m_testdraw.DrawUsingCamera(trans2, 1);
+    //  m_GameObjectManager.Update();
+}
