ae90c3bfdbf7a49342d52e20a0f73a0be0e71935
jonylu7 <jonylu7@gmail.com>
2024-03-23 15:23:43 +0800

    attack and damage

---
+++ include/Unit/AttackAndDamageUnit.hpp
@@ -0,0 +1,45 @@
+//
+// Created by 盧威任 on 3/22/24.
+//
+
+#ifndef PRACTICALTOOLSFORSIMPLEDESIGN_ATTACKANDDAMAGEUNIT_HPP
+#define PRACTICALTOOLSFORSIMPLEDESIGN_ATTACKANDDAMAGEUNIT_HPP
+#include "Avatar.hpp"
+class AttackAndDameageUnit {
+public:
+    AttackAndDameageUnit() {}
+    ~AttackAndDameageUnit() {}
+
+    int getHP() { return m_HP; }
+    int getArmorRate() { return m_ArmorRate; }
+    int getSoftAttack() { return m_SoftAttack; }
+    int getHardAttack() { return m_HardAttack; }
+
+    void setHP(int hp) { m_HP = hp; }
+    void setArmorRate(int armor) { m_ArmorRate = armor; }
+    void setSoftAttack(int softattack) { m_SoftAttack = softattack; }
+    void setHardAttack(int hardattack) { m_SoftAttack = hardattack; }
+
+    void takeDamage(int softattack, int hardattack) {
+
+        m_HP -= (100 - m_ArmorRate) * (1 / 100) * softattack +
+                m_ArmorRate * (1 / 100) * hardattack;
+    }
+
+    void ForceAttackUnit(std::shared_ptr<AttackAndDameageUnit> target) {
+        // check withinrange
+        // cd time
+        target->takeDamage(m_SoftAttack, m_HardAttack);
+        return;
+    }
+
+private:
+    int m_HP;
+    float m_ArmorRate;
+    int m_SoftAttack;
+    int m_HardAttack;
+    float m_AttackRange;
+    int cd;
+};
+
+#endif // PRACTICALTOOLSFORSIMPLEDESIGN_ATTACKANDDAMAGEUNIT_HPP
