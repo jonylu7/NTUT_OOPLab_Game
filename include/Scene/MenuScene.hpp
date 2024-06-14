782b1bdffa692d3fb816b57bb13ba07c2df862b8
jonylu7 <jonylu7@gmail.com>
2024-06-14 16:38:56 +0800

    win or failed

--- include/Scene/MenuScene.hpp
+++ include/Scene/MenuScene.hpp
@@ -33,8 +33,6 @@ public:
                 m_BGM.Play();
             }
             if (Util::Input::IsKeyPressed(Util::Keycode::K)) {
-                m_BGM.LoadMedia("../assets/BGM/Jimmy_Scott-Sycamore_Trees.mp3");
-                m_BGM.Play();
             }
             if (Util::Input::IsKeyPressed(Util::Keycode::M)) {
                 m_CurrentMode = SceneMode::MAP;
@@ -47,22 +45,26 @@ public:
                     UIStatusType::UI_START) {
                 m_MenuUI.getUIStatus()->setUIStatusType(
                     UIStatusType::UI_DEFAULT);
+                m_BGM.Play();
                 m_CurrentMode = SceneMode::TUTORIAL;
             }
             if (Util::Input::IsKeyPressed(Util::Keycode::S)) {
                 m_CurrentMode = SceneMode::SANDBOX;
             }
-            if (m_TutorialScene->getMissionAccomplishedUI()
+        }
+        if (m_CurrentMode == SceneMode::TUTORIAL &&
+            m_TutorialScene->getMissionAccomplishedUI()
                     ->getUIStatus()
                     ->getUIStatusType() == UIStatusType::UI_CONTINUE) {
-                m_CurrentMode = SceneMode::SKIRMISH;
-            }
+            m_MenuUI.getUIStatus()->setUIStatusType(UIStatusType::UI_DEFAULT);
+            m_SandBoxScene->setStage(true);
+            m_CurrentMode = SceneMode::SANDBOX;
         }
-        if (m_CurrentMode==SceneMode::TUTORIAL&&m_TutorialScene->getMissionAccomplishedUI()
-                ->getUIStatus()
-                ->getUIStatusType() == UIStatusType::UI_CONTINUE) {
-            m_MenuUI.getUIStatus()->setUIStatusType(
-                UIStatusType::UI_DEFAULT);
+
+        if (m_CurrentMode == SceneMode::SANDBOX &&
+            m_SandBoxScene->getWonUI()->getUIStatus()->getUIStatusType() ==
+                UIStatusType::UI_RESTART) {
+            m_SandBoxScene->Start();
             m_SandBoxScene->setStage(true);
             m_CurrentMode = SceneMode::SANDBOX;
         }
@@ -97,7 +99,9 @@ public:
                 UIStatusType::UI_EXIT ||
             m_TutorialScene->getMissionAccomplishedUI()
                     ->getUIStatus()
-                    ->getUIStatusType() == UIStatusType::UI_EXIT) {
+                    ->getUIStatusType() == UIStatusType::UI_EXIT ||
+            m_SandBoxScene->getWonUI()->getUIStatus()->getUIStatusType() ==
+                UIStatusType::UI_EXIT) {
             return true;
         }
         return false;
--- include/UI/WonUI.hpp
+++ include/UI/WonUI.hpp
@@ -2,16 +2,17 @@
 // Created by 盧威任 on 5/25/24.
 //
 
-#ifndef PRACTICALTOOLSFORSIMPLEDESIGN_MISSIONACCOMPLISHED_HPP
-#define PRACTICALTOOLSFORSIMPLEDESIGN_MISSIONACCOMPLISHED_HPP
+#ifndef PRACTICALTOOLSFORSIMPLEDESIGN_WUI_HPP
+#define PRACTICALTOOLSFORSIMPLEDESIGN_WUI_HPP
+#include "Button.hpp"
 #include "UI/UI.hpp"
-class MissionFailedUI : public UI {
+class WonUI : public UI {
 public:
-    MissionFailedUI() {}
-    ~MissionFailedUI() {}
+    WonUI() {}
+    ~WonUI() {}
     void Update() override {
         if (m_ContinueButton->ifClicked()) {
-            m_UIStatus->setUIStatusType(UIStatusType::UI_CONTINUE);
+            m_UIStatus->setUIStatusType(UIStatusType::UI_RESTART);
         } else if (m_ExitButton->ifClicked()) {
             m_UIStatus->setUIStatusType(UIStatusType::UI_EXIT);
         }
@@ -20,16 +21,22 @@ public:
 
     void Draw() override {
         m_PausedImage->Draw(
-            Util::Transform({glm::vec2(0, 70), float(0), glm::vec2(1, 1)}), 1);
+            Util::Transform({glm::vec2(0, 70), float(0), glm::vec2(0.8, 0.8)}),
+            1);
         m_ContinueButton->Draw();
         m_ExitButton->Draw();
     }
 
+    void setPausedImage(std::shared_ptr<Util::Image> image) {
+        m_PausedImage = image;
+    }
+
 private:
     std::shared_ptr<Util::Image> m_PausedImage =
-        std::make_shared<Util::Image>("../assets/images/paused.png");
+        std::make_shared<Util::Image>("../assets/images/PlayerWon.png");
+
     std::shared_ptr<Button> m_ContinueButton = std::make_shared<Button>(
-        std::make_shared<Util::Image>("../assets/images/continue.png"), 1, true,
+        std::make_shared<Util::Image>("../assets/images/restart.png"), 1, true,
         Util::Transform({glm::vec2(300, -100), float(0), glm::vec2(0.4, 0.4)}));
 
     std::shared_ptr<Button> m_ExitButton = std::make_shared<Button>(
@@ -37,4 +44,4 @@ private:
         Util::Transform(
             {glm::vec2(-300, -100), float(0), glm::vec2(0.4, 0.4)}));
 };
-#endif // PRACTICALTOOLSFORSIMPLEDESIGN_MISSIONACCOMPLISHED_HPP
+#endif // PRACTICALTOOLSFORSIMPLEDESIGN_WUI_HPP
